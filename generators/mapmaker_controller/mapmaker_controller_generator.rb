9d4b1e3d1320202cfd37bf650898772b490a7059
John Barton <jrbarton@gmail.com>
2008-06-21 00:39:44 +1000

    adding motherfucking generators

---
+++ generators/mapmaker_controller/mapmaker_controller_generator.rb
@@ -0,0 +1,8 @@
+require 'rails_generator/generators/components/controller/controller_generator'
+
+class MapmakerControllerGenerator < ControllerGenerator
+  def manifest
+    record do |m|
+    end
+  end
+end
